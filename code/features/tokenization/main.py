dfa265fa0dec00f8d096f09b02c014ae3c826f3e
rb58853 <rb58853@gmail.com>
2023-12-03 20:10:55 -0500

    save

--- code/features/tokenization/main.py
+++ code/features/tokenization/main.py
@@ -1,17 +1,12 @@
 from gramatic.parser import CaptionLexer,CaptionParser
 
 caption0 = 'a dog eating fruit two cats playing in the snow.'
-# caption1 = 'a dog and two cats playing'
 caption1 = 'a tall dog and two cats playing in the snow.'
-# caption2='an orange couch'
-# caption2 = 'two cats and a dog sitting on an orange couch, dog with a blue collar and a brown nose'
-caption2='two cats and a dog sitting on an orange couch, a cartoon dog laying on a pillow, a cartoon cat with a smile on its face, cartoon illustration of a cat and dog lying on a couch cartoon, dog with a blue collar and a brown nose'
+caption2 = 'two cats and a dog sitting on an orange couch, dog with a blue collar and a brown nose'
+# caption2='two cats and a dog sitting on an orange couch, a cartoon dog laying on a pillow, a cartoon cat with a smile on its face, cartoon illustration of a cat and dog lying on a couch cartoon, dog with a blue collar and a brown nose'
 
 lexer = CaptionLexer()
 parser = CaptionParser()
-
-# for token in lexer.tokenize(caption2):
-#     print(f'{token.value} ({token.type})')
 
 parser.parse(lexer.tokenize(caption2)) 
 for t in parser.sintagmas:
