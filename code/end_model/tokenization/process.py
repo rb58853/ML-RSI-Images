ac1d23a293ed798edf602968e4865579f913e204
rb58853 <rb58853@gmail.com>
2023-12-19 18:40:15 -0500

    Update Parser

--- code/end_model/tokenization/process.py
+++ code/end_model/tokenization/process.py
@@ -15,10 +15,11 @@ def get_keys(caption):
                 parser_right.parse(lexer.tokenize(caption[i:]))
     
     #Esto reduce siempre a la izquierda, esto sirve cuando hay que reducir por ejemplo:  `adj and adj noun`, aqui ambos adjetivos modifican al sustantivo
+    parser_left.sintagmas = parser_right.sintagmas
     for caption in captions:
         parser_left.parse(lexer.tokenize(caption))
         for i in range(len(caption)):
             if caption[i] == " ":
                 parser_left.parse(lexer.tokenize(caption[i:]))
 
-    return parser_left.sintagmas + parser_right.sintagmas
+    return parser_left.sintagmas
