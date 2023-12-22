d459a37a6419cf83d713e2d910d850bdd5fb5148
rb58853 <rb58853@gmail.com>
2023-12-21 20:15:23 -0500

    Show_images added

--- code/CLIP-Embedding/clip_embeding.py
+++ code/CLIP-Embedding/clip_embeding.py
@@ -4,11 +4,12 @@ from scipy.spatial.distance import cosine
 from transformers import CLIPProcessor, CLIPModel
 from sam import SAM
 import cv2
+import matplotlib.pyplot as plt
 
 class ClipEmbedding():
     def __init__(self) -> None:
         self.model, self.processor, self.device = self.get_model()
-
+        
     def get_model(self):    
         device = "cuda" if torch.cuda.is_available() else "cpu"
         model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
@@ -16,7 +17,7 @@ class ClipEmbedding():
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         model = model.to(device)
         return(model, processor, device)
-    
+
     def get_image_embedding(self, image):
         image_process = self.processor(
            text=None,
@@ -57,7 +58,7 @@ class ClipEmbedding():
 
 
 class ProcessImages:
-    IMAGE_PARTITION = 64 #tamaño mínimo(en píxeles) de un cuadro de segmentación = tamaño(imagen)/IMAGE_PARTITION
+    IMAGE_PARTITION = 80 #tamaño mínimo(en píxeles) de un cuadro de segmentación = tamaño(imagen)/IMAGE_PARTITION
     SEGMENTATION = 'box' #tipo de segmentacion a usar en sam
 
     def __init__(self) -> None:
@@ -65,14 +66,16 @@ class ProcessImages:
         self.sam = SAM
         self.sam.import_model()
         self.AREA = 20*20
+        self.segmentations = []
 
     def get_segmentation_images(self, image_path):
+        self.segmentations = []
         image = self.load_cv2_image(image_path)
         return self.sam.all_areas_from_image(image,min_box_area = self.AREA, min_area = self.AREA/2)[ProcessImages.SEGMENTATION]
 
     def get_embedding_segmentations(self, image_path):
-        segmentations = self.get_segmentation_images(image_path)
-        return [self.clip.get_image_embedding(image) for image in segmentations]
+        self.segmentations = self.get_segmentation_images(image_path)
+        return [self.clip.get_image_embedding(image) for image in self.segmentations]
     
     def full_embeddings_from_image(self, image_path):
         image = self.load_pil_image(image_path)
@@ -110,5 +113,18 @@ class ProcessImages:
                 print(f'index_{indexs[key]}: {key}')
         
         return result
+    
+    def show_images(self, image_path = None):
+        if len(self.segmentations) == 0:
+            self.segmentations = self.get_segmentation_images(image_path)
+        
+        index = 1
+        for im in self.segmentations:
+            plt.figure(figsize=(2,2))
+            plt.title(index)
+            plt.imshow(im)
+            plt.axis('off')
+            plt.show()
+            index+=1
 
 
